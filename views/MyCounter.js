c8400d9eff12e946b2c45326cb8cf099c92ca6db
renato1958 <felisi.renato@gmail.com>
2022-04-09 16:39:45 +0200

    Aggiunti pulsanti e relative funzioni

--- views/MyCounter.js
+++ views/MyCounter.js
@@ -1,5 +1,26 @@
 export default {
   	name: 'MyCounter',
-	template: `<h1>Ciao a tutti!</h1>`
+	template: `<h3>La conta ammonta ora a {{ conta }}</h3>
+				<h4 v-if="conta === 10"> Raggiunto il massimo consentito!</h4>
+				<h4 v-if="conta === -10"> Raggiunto il minimo consentito!</h4>
+               <button @click="decrementa">Decrementa (min -10)</button>
+               <button @click="incrementa">Incrementa (max 10)</button>`,
+  	data() {
+		return {
+			conta: 0
+		}
+	},
+  	methods: {
+		incrementa() {
+			if(this.conta < 10) {
+				this.conta++
+			}		
+		},
+	  	decrementa() {
+			if(this.conta > -10) {
+				this.conta--
+			}
+		}
+	}  
 }
 
