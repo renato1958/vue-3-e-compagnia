12e7226fab46580edfaf392c7d006cdda46b76b9
renato1958 <felisi.renato@gmail.com>
2022-04-11 17:34:17 +0200

    Aggiunto contenitore generale a mycounter

--- views/MyCounter.js
+++ views/MyCounter.js
@@ -1,10 +1,12 @@
 export default {
   	name: 'MyCounter',
-	template: `<h3>La conta ammonta ora a {{ conta }}</h3>
-				<h4 v-if="conta === 10"> Raggiunto il massimo consentito!</h4>
-				<h4 v-if="conta === -10"> Raggiunto il minimo consentito!</h4>
-               <button @click="decrementa">Decrementa (min -10)</button>
-               <button @click="incrementa">Incrementa (max 10)</button>`,
+	template: `<div class="main-container">
+              <h3>La conta ammonta ora a {{ conta }}</h3>
+				      <h4 v-if="conta === 10"> Raggiunto il massimo consentito!</h4>
+				      <h4 v-if="conta === -10"> Raggiunto il minimo consentito!</h4>
+              <button @click="decrementa">Decrementa (min -10)</button>
+              <button @click="incrementa">Incrementa (max 10)</button>
+             </div><!-- .main-container -->`,
   	data() {
 		return {
 			conta: 0
